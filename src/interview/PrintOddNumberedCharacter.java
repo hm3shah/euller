9256fa2838aa6776605179759fe20b5b2c4009aa
hshah <hshah@zynga.com>
2016-12-10 12:36:14 -0500

    - code formatting

--- src/interview/PrintOddNumberedCharacter.java
+++ src/interview/PrintOddNumberedCharacter.java
@@ -4,38 +4,37 @@ package interview;
 import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 
-import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
 import static junit.framework.TestCase.assertEquals;
 
 /**
-Given string write code that generate the output
-
-        aaaabbb -> b
-        aaabbbcccdd -> abc
-        abc -> abc
-        aabbbccdda -> ba
+ * Given string write code that generate the output
+ * <p>
+ * aaaabbb -> b
+ * aaabbbcccdd -> abc
+ * abc -> abc
+ * aabbbccdda -> ba
  **/
 public class PrintOddNumberedCharacter {
 
     public String getOddNumberedCharacter(String string) {
-        if(StringUtils.isEmpty(string)) {
+        if (StringUtils.isEmpty(string)) {
             return string;
         } else {
             char[] characters = string.toCharArray();
             Set<Character> charSet = new LinkedHashSet<>();
-            for(int i=0; i < characters.length; ++i) {
+            for (int i = 0; i < characters.length; ++i) {
                 char temp = characters[i];
-                if(charSet.contains(temp)) {
+                if (charSet.contains(temp)) {
                     charSet.remove(temp);
                 } else {
                     charSet.add(temp);
                 }
             }
             StringBuilder sb = new StringBuilder();
-            for(Character c : charSet) {
+            for (Character c : charSet) {
                 sb.append(c);
             }
             return sb.toString();
@@ -45,12 +44,12 @@ public class PrintOddNumberedCharacter {
     @Test
     public void test() {
         PrintOddNumberedCharacter printOddNumberedCharacter = new PrintOddNumberedCharacter();
-        assertEquals(null,printOddNumberedCharacter.getOddNumberedCharacter(null));
-        assertEquals("",printOddNumberedCharacter.getOddNumberedCharacter(""));
-        assertEquals("b",printOddNumberedCharacter.getOddNumberedCharacter("aaaabbb"));
-        assertEquals("abc",printOddNumberedCharacter.getOddNumberedCharacter("aaabbbcccdd"));
-        assertEquals("abc",printOddNumberedCharacter.getOddNumberedCharacter("abc"));
-        assertEquals("ba",printOddNumberedCharacter.getOddNumberedCharacter("aabbbccdda"));
+        assertEquals(null, printOddNumberedCharacter.getOddNumberedCharacter(null));
+        assertEquals("", printOddNumberedCharacter.getOddNumberedCharacter(""));
+        assertEquals("b", printOddNumberedCharacter.getOddNumberedCharacter("aaaabbb"));
+        assertEquals("abc", printOddNumberedCharacter.getOddNumberedCharacter("aaabbbcccdd"));
+        assertEquals("abc", printOddNumberedCharacter.getOddNumberedCharacter("abc"));
+        assertEquals("ba", printOddNumberedCharacter.getOddNumberedCharacter("aabbbccdda"));
     }
 
 }
